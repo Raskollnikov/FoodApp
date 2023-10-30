8abd2e29a3fb42cd7d1dfa43d0f94ef64e8cc3b8
Raskollnikov <bakhalaia56@gmail.com>
2023-10-30 23:46:40 +0400

    error page/router

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,17 +1,7 @@
-import { useRouteError } from "react-router-dom";
+import React from "react";
 
-const Error = () => {
-  const err = useRouteError();
-  console.log(err);
-  return (
-    <div>
-      <h1>OOps</h1>
-      <p>something Went Wrong!</p>
-
-      <p>{err.data}</p>
-      <h3>{err.status}</h3>
-    </div>
-  );
-};
+function Error() {
+  return <div></div>;
+}
 
 export default Error;
