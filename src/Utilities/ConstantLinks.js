395dce4dfa18375d98996cd8f32913e4b3cc24f0
Raskollnikov <bakhalaia56@gmail.com>
2023-11-29 13:33:55 +0400

    api changed

--- src/Utilities/ConstantLinks.js
+++ src/Utilities/ConstantLinks.js
@@ -5,4 +5,5 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const swiggy_api_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+  // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
--- src/components/ResCategory.1.jsx
+++
@@ -1,3 +0,0 @@
-export function ResCategory() {
-  return <div></div>;
-}
